0aa7727414674cc218cb655923f3f87865b1dd2a
Wellik <jwellik@usgs.gov>
2025-12-14 11:12:02 -0800

    init TODO.py

---
+++ TODO.py
@@ -0,0 +1,19 @@
+## Build
+# TODO Stabilize/install camera and Raspberry Pi
+# TODO Install printer
+# TODO $ python3 test_music_butler.py
+# - Start playing an album and print sticker
+# TODO Install rotary encoder
+# TODO Make cards
+
+
+## QR Codes
+# TODO Better documentation for generate_qr_codes.py
+# TODO $ python3 generate_qr_codes.py {uri}
+
+
+
+
+## Progress Tracker
+# 2025-12-11: PROGRESS: Recognizes a Spotify URI via QR code
+# 2025-12-11: NEXT: Get printer to work. Spotify authentication.
